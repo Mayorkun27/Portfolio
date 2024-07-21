86154d8e4d1def7d8c597ebafaf93cdee8cf27e0
Adeleke Favour Oluwamayokun <156526127+Mayorkun27@users.noreply.github.com>
2024-07-21 14:48:57 +0100

    Added files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+AOS.init();
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   setTimeout(function() {
+//     document.querySelector('.terminal-loader').style.display = 'none';
+//     document.querySelector('.mainContent').style.display = 'block';
+//   }, 1000);
+// });
+
+let prevScrollPos = window.pageYOffset;
+const backToTop = document.querySelector(".backToTop");
+
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollPos < currentScrollPos) {
+    backToTop.style.display = "grid"
+  }
+  else if (currentScrollPos = "0") {
+    backToTop.style.display = "none"
+  }
+  prevScrollPos = currentScrollPos;
+}
